fb747ffc560025b0cf314a7c9f877d74acf6d690
Aviral Srivastava <63746541+Aviral-3@users.noreply.github.com>
2021-07-03 03:54:09 +0530

    Add files via upload

---
+++ Hello world.cs
@@ -0,0 +1,16 @@
+using System;
+namespace HelloWorldApp {
+      class Geeks {
+          
+        // Main Method
+        static void Main(string[] args) {
+              
+     
+            // printing Hello World!
+            Console.WriteLine("Hello World!");
+              
+       
+            Console.ReadKey();
+        }
+    }
+}
